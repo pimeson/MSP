67e82a7e0eaa3bb9ab0b775c0aa2f7ae0cd9a2d7
Naohito Sadoshima <nsadoshima@gmail.com>
2016-12-02 01:19:26 -0600

    fixed?

--- Server/app/routes/exhibit/index.js
+++ Server/app/routes/exhibit/index.js
@@ -8,7 +8,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './public/upload/')
+    cb(null, './public/uploads/')
   },
   filename: (req, file, cb) => {
     console.log(file);
