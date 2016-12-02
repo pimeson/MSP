6d28303922339ce2ef4c2c9cff145a89ab7378a9
Naohito Sadoshima <nsadoshima@gmail.com>
2016-12-02 01:06:43 -0600

    caps

--- Server/env/index.js
+++ Server/env/index.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var devConfigPath = path.join(__dirname, './development.js');
-var productionConfigPath = path.join(__dirname, '../../server/env/production.js');
+var productionConfigPath = path.join(__dirname, '../../Server/env/production.js');
 // var testConfigPath = path.join(__dirname, './testing.js');
 
 if (process.env.NODE_ENV === 'production') {
