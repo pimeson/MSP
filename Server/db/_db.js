356cd981cd8028c2fc3c3231d86e02c27d183d57
Naohito Sadoshima <nsadoshima@gmail.com>
2016-12-09 19:05:45 -0600

    take 3?

--- Server/db/_db.js
+++ Server/db/_db.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const Sequelize = require('sequelize');
-const dbURL = 'postgres://localhost:5432/msp';
+const dbURL = 'postgres://postgres@localhost:5432/msp';
 
 // const env = require(path.join(__dirname, '../env'));
 // const url = env.DATABASE_URL
 const url = dbURL
-const db = new Sequelize(url, 'postgres', 'postgres', {
+const db = new Sequelize('msp', 'postgres', 'postgres', {
   // logging: env.LOGGING ? console.log : false,
   // native: env.NATIVE
   logging: false,
