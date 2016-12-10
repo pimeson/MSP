5bbb21bbfb4e08310390b9a2d6866b8d14e7d122
Naohito Sadoshima <nsadoshima@gmail.com>
2016-12-09 18:53:50 -0600

    changes for DO

--- Server/db/_db.js
+++ Server/db/_db.js
@@ -1,10 +1,16 @@
 const path = require('path');
 const Sequelize = require('sequelize');
+const dbURL = 'postgres://localhost:5432/msp';
 
-const env = require(path.join(__dirname, '../env'));
-const db = new Sequelize(env.DATABASE_URL, {
-  logging: env.LOGGING ? console.log : false,
-  native: env.NATIVE
+// const env = require(path.join(__dirname, '../env'));
+// const url = env.DATABASE_URL
+const url = dbURL
+const db = new Sequelize(url, 'postgres', '', {
+  // logging: env.LOGGING ? console.log : false,
+  // native: env.NATIVE
+  logging: false,
+  hostname: 'localhost',
+  dialect: 'postgres'
 })
 
 module.exports = db;
