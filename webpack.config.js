68a7169449ce5ab1716b92869a228e2b563d80ca
Naohito Sadoshima <nsadoshima@gmail.com>
2017-01-07 14:45:54 -0600

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,34 +9,36 @@ module.exports = {
     path: __dirname + '/Browser/js',
     filename: 'bundle.js'
   },
-  loaders: [{
-    test: /\.js$/,
-    loader: 'babel-loader',
-    exclude: [/node_modules/],
-    query: {
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: [/node_modules/],
+      query: {
         presets: ['es2015']
       }
-  }, {
-    test: /\.html$/,
-    loader: 'html-loader',
-    exclude: [/node_modules/]
-  }, {
-    test: /\.css$/,
-    loader: "style!css",
-    exclude: [/node_modules/]
-  }, {
-    test: /\.scss$/,
-    loader: "style!css!sass",
-    exclude: [/node_modules/]
-  }],
-  plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery"
-        })
+    }, {
+      test: /\.html$/,
+      loader: 'html-loader',
+      exclude: [/node_modules/]
+    }, {
+      test: /\.css$/,
+      loader: "style!css",
+      exclude: [/node_modules/]
+    }, {
+      test: /\.scss$/,
+      loader: "style!css!sass",
+      exclude: [/node_modules/]
+    }],
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
+      })
     ],
-  node: {
-  fs: "empty"
+    node: {
+      fs: "empty"
+    }
   }
 };
