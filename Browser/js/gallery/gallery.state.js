680bbd332b7040c9e7329427215e2b8faecb248b
Naohito Sadoshima <nsadoshima@gmail.com>
2021-01-22 21:20:24 -0500

    rename uri to project

--- Browser/js/gallery/gallery.state.js
+++ Browser/js/gallery/gallery.state.js
@@ -1,19 +1,19 @@
-module.exports = function(app) {
+module.exports = function (app) {
 
-app.config( $stateProvider => {
-  $stateProvider.state('gallery', {
-    url: '/gallery/:projId',
-    controller: 'GalleryCtrl',
-    templateUrl: 'js/gallery/gallery.html',
-    resolve: {
-      exhibits: function($stateParams, exhibitFactory){
-        return exhibitFactory.findAllByProjectId($stateParams.projId);
-      },
-      project: function($stateParams, projectFactory){
-        return projectFactory.findById($stateParams.projId);
+  app.config($stateProvider => {
+    $stateProvider.state('gallery', {
+      url: '/project/:projId',
+      controller: 'GalleryCtrl',
+      templateUrl: 'js/gallery/gallery.html',
+      resolve: {
+        exhibits: function ($stateParams, exhibitFactory) {
+          return exhibitFactory.findAllByProjectId($stateParams.projId);
+        },
+        project: function ($stateParams, projectFactory) {
+          return projectFactory.findById($stateParams.projId);
+        }
       }
-    }
+    })
   })
-})
 
 }
